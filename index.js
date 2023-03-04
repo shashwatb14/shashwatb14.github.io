9f374d9744276f21103c67361c050b1e08e61cda
Shashwat <shashwat.bhandari.14@gmail.com>
2023-03-04 12:57:10 +0800

    Update h1

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 function toggleMobileMenu(menu) {
     menu.classList.toggle('open');
 }
+
+// hyperplexed hacker text effect
+window.onload = function(){ 
+    const letters = "abcdefghijklmnopqrstuvwxyz";
+
+    document.querySelector("h1").onmouseover = event => {
+        let iterations = 0;
+
+        const interval = setInterval(() => {
+            event.target.innerText = event.target.innerText.split("")
+            .map((letter, index) => {
+                if (index < iterations) {
+                    return event.target.dataset.value[index];
+                }
+
+                return letters[Math.floor(Math.random() * 26)]
+            })
+            .join("");
+
+        if (iterations >= 9) clearInterval(interval);
+
+        iterations += 1 / 2;
+        }, 40);
+    }   
+};
