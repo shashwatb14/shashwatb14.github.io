ef6f987d93cb9e7a29c766f2f1bced3979771c29
Shashwat <shashwat.bhandari.14@gmail.com>
2023-03-04 16:19:07 +0800

    Update mouse trailer

--- index.js
+++ index.js
@@ -28,17 +28,24 @@ window.onload = function() {
 
     const trailer = document.getElementById("trailer");
 
-    window.onmousemove = e => {
+    const animateTrailer = (e, interacting) => {
         const x = e.clientX - trailer.offsetWidth / 2,
             y = e.clientY - trailer.offsetHeight / 2;
 
         const keyframes = {
-            transform: `translate(${x}px, ${y}px)`
+            transform: `translate(${x}px, ${y}px) scale(${interacting ? 0.5 : 1})`,
         }
 
         trailer.animate(keyframes, {
             duration: 800,
             fill: "forwards"
         });
+    }
+
+    window.onmousemove = e => {
+        const interactable = e.target.closest("a"),
+              interacting = interactable !== null;
+        
+        animateTrailer(e, interacting);
     }
 };
