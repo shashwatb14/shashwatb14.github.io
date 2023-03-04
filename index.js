749236840cf4ea7dde448efe6b5dd06dabf76999
Shashwat <shashwat.bhandari.14@gmail.com>
2023-03-04 16:01:39 +0800

    Add mouse trailer

--- index.js
+++ index.js
@@ -24,5 +24,21 @@ window.onload = function() {
 
         iterations += 1 / 2;
         }, 40);
+    };
+
+    const trailer = document.getElementById("trailer");
+
+    window.onmousemove = e => {
+        const x = e.clientX - trailer.offsetWidth / 2,
+            y = e.clientY - trailer.offsetHeight / 2;
+
+        const keyframes = {
+            transform: `translate(${x}px, ${y}px)`
+        }
+
+        trailer.animate(keyframes, {
+            duration: 800,
+            fill: "forwards"
+        });
     }
 };
