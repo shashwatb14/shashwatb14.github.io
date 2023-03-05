31369dccf7b245872f41013d54da31e6f7d18076
Shashwat <shashwat.bhandari.14@gmail.com>
2023-03-05 14:40:23 +0800

    Update favicon and add text effects

--- index.js
+++ index.js
@@ -2,9 +2,30 @@ function toggleMobileMenu(menu) {
     menu.classList.toggle('open');
 }
 
-// hyperplexed hacker text effect
+// hyperplexed effects
 window.onload = function() { 
-    const letters = "abcdefghijklmnopqrstuvwxyz";
+    const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    document.querySelector("a").onmouseover = event => {
+        console.log("works");
+        let iterations = 0;
+
+        const interval = setInterval(() => {
+            event.target.innerText = event.target.innerText.split("")
+            .map((letter, index) => {
+                if (index < iterations) {
+                    return event.target.dataset.value[index];
+                }
+
+                return letters[Math.floor(Math.random() * 52)]
+            })
+            .join("");
+
+        if (iterations >= 9) clearInterval(interval);
+
+        iterations += 1 / 2;
+        }, 20);
+    };
 
     document.querySelector("h1").onmouseover = event => {
         let iterations = 0;
@@ -16,7 +37,7 @@ window.onload = function() {
                     return event.target.dataset.value[index];
                 }
 
-                return letters[Math.floor(Math.random() * 26)]
+                return letters[Math.floor(Math.random() * 52)]
             })
             .join("");
 
