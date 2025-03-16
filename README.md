76ccc40b23f469ab38c27bc08685f9edc8a1f1b0
shashwat <shashwat.bhandari.14@gmail.com>
2025-03-16 15:47:44 -0700

    update

--- README.md
+++ README.md
@@ -5,4 +5,3 @@
 ---
 
 My Personal Portfolio Website (initially started as an assignment part of the CS50x Week 8 Homepage).
-
--- script.js
+++ script.js
@@ -1,94 +1,97 @@
 function toggleMobileMenu(menu) {
-    menu.classList.toggle('open');
+  menu.classList.toggle("open");
 }
 
 // hyperplexed effects
-window.onload = function() { 
-    const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ<>";
-    const elements = document.querySelectorAll("a");
-    
-    if (elements) {
-        elements.forEach(function(element) {
-            if (element.parentElement.className === "ref-link") return;
-            element.onmouseover = event => {
-                let iterations = 0;
+window.onload = function () {
+  const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ<>";
+  const elements = document.querySelectorAll("a");
 
-                if (!event.target.dataset.value) return;
+  if (elements) {
+    elements.forEach(function (element) {
+      if (element.parentElement.className === "ref-link") return;
+      element.onmouseover = (event) => {
+        let iterations = 0;
 
-                const interval = setInterval(() => {
-                    event.target.innerText = event.target.innerText.split("")
-                    .map((letter, index) => {
-                        if (index < iterations) {
-                            return event.target.dataset.value[index];
-                        }
+        if (!event.target.dataset.value) return;
 
-                        return letters[Math.floor(Math.random() * 54)]
-                    })
-                    .join("");
+        const interval = setInterval(() => {
+          event.target.innerText = event.target.innerText
+            .split("")
+            .map((letter, index) => {
+              if (index < iterations) {
+                return event.target.dataset.value[index];
+              }
 
-                if (iterations >= event.target.dataset.value.length) clearInterval(interval);
+              return letters[Math.floor(Math.random() * 54)];
+            })
+            .join("");
 
-                iterations += 1;
-                }, 20);
-            };
-        });
-    }
+          if (iterations >= event.target.dataset.value.length)
+            clearInterval(interval);
 
-    var heading = document.querySelector("h1");
+          iterations += 1;
+        }, 20);
+      };
+    });
+  }
 
-    if(heading) {
+  var heading = document.querySelector("h1");
 
-        heading.onmouseover = event => {
-            let iterations = 0;
+  if (heading) {
+    heading.onmouseover = (event) => {
+      let iterations = 0;
 
-            const interval = setInterval(() => {
-                event.target.innerText = event.target.innerText.split("")
-                .map((letter, index) => {
-                    if (index < iterations) {
-                        return event.target.dataset.value[index];
-                    }
-
-                    return letters[Math.floor(Math.random() * 52)]
-                })
-                .join("");
-
-            if (iterations >= event.target.dataset.value.length) clearInterval(interval);
-
-            iterations += 1 / 2;
-            }, 40);
-        }
-    }
-
-    const trailer = document.getElementById("trailer");
-
-    const animateTrailer = (e, interacting) => {
-        const x1 = e.clientX - trailer.offsetWidth / 2,
-              y1 = e.clientY - trailer.offsetHeight / 2;
-
-        let keyframes1 = {}
-        
-        if (interacting) {
-            keyframes1 = {
-                transform: `translate(${x1}px, ${y1}px) scale(${interacting ? 4 : 1})`,
-                backgroundColor: `transparent`
+      const interval = setInterval(() => {
+        event.target.innerText = event.target.innerText
+          .split("")
+          .map((letter, index) => {
+            if (index < iterations) {
+              return event.target.dataset.value[index];
             }
-        } else {
-            keyframes1 = {
-                transform: `translate(${x1}px, ${y1}px) scale(${interacting ? 4 : 1})`,
-                backgroundColor: `var(--color-tertiary)`
-            }
-        }
 
-        trailer.animate(keyframes1, {
-            duration: 800,
-            fill: "forwards"
-        });
+            return letters[Math.floor(Math.random() * 52)];
+          })
+          .join("");
+
+        if (iterations >= event.target.dataset.value.length)
+          clearInterval(interval);
+
+        iterations += 1 / 2;
+      }, 40);
+    };
+  }
+
+  const trailer = document.getElementById("trailer");
+
+  const animateTrailer = (e, interacting) => {
+    const x1 = e.clientX - trailer.offsetWidth / 2,
+      y1 = e.clientY - trailer.offsetHeight / 2;
+
+    let keyframes1 = {};
+
+    if (interacting) {
+      keyframes1 = {
+        transform: `translate(${x1}px, ${y1}px) scale(${interacting ? 4 : 1})`,
+        backgroundColor: `transparent`,
+      };
+    } else {
+      keyframes1 = {
+        transform: `translate(${x1}px, ${y1}px) scale(${interacting ? 4 : 1})`,
+        backgroundColor: `var(--color-tertiary)`,
+      };
     }
 
-    window.onmousemove = e => {
-        const interactable = e.target.closest(".interactable"),
-              interacting = interactable !== null;
-        
-        animateTrailer(e, interacting);
-    }
-}
+    trailer.animate(keyframes1, {
+      duration: 800,
+      fill: "forwards",
+    });
+  };
+
+  window.onmousemove = (e) => {
+    const interactable = e.target.closest(".interactable"),
+      interacting = interactable !== null;
+
+    animateTrailer(e, interacting);
+  };
+};
